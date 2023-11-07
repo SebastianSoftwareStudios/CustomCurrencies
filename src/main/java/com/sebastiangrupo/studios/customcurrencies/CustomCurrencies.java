39da3eab0a65a6d43c4c16d16fbaca9a22ad373f
BuildTools <sebastianrivera14@outlook.com>
2023-11-07 13:47:18 -0500

    First commit

---
+++ src/main/java/com/sebastiangrupo/studios/customcurrencies/CustomCurrencies.java
@@ -0,0 +1,17 @@
+package com.sebastiangrupo.studios.customcurrencies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomCurrencies extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
